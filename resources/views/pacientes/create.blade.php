f4ac8c1e8f3608ff2893477e197b517aad8b7e15
antajara <tatisjamauk@gmail.com>
2023-10-04 15:40:02 -0500

    estilos css

--- resources/views/pacientes/create.blade.php
+++ resources/views/pacientes/create.blade.php
@@ -5,18 +5,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <h1>CREAR PACIENTE</h1>
-    <form action="{{route('pacientes.store')}}" method="post">
-        @csrf
-        Nombre:  <input type="text" name="nombre"><br>
-        Apellido:<input type="text" name="apellido"><br>
-        Celular: <input type="text" name="celular"><br>
-        <input type="submit" value="Agregar Paciente">
-    </form>
+
+    <div class= "container p-4">
+        <div class= "row">
+            <div class= "col-md-4">
+                <div class= "card card-body">
+                    <h1>CREAR PACIENTE</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <div class= "container p-4">
+        <div class= "row">
+            <div class= "col-md-4">
+                <div class= "card card-body">
+                    <form action="{{route('pacientes.store')}}" method="post">
+                        @csrf
+                        Nombre:  <input type="text" name="nombre" class="form-control" placeholder="Ingrese su Nombre" required><br>
+                        Apellido:<input type="text" name="apellido" class="form-control" required><br>
+                        Celular: <input type="text" name="celular" class="form-control" required><br>
+                        <input type="submit" value="Agregar Paciente" class="btn btn-success btn-block">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+   
 
     <br>
-    <a href="{{ route('pacientes.index') }}">REGRESAR A  PACIENTES</a>
+
+    
+    <div class= "container p-4">
+        <div class= "row">
+            <div class= "col-md-4">
+                <div class= "card card-body">
+                    <a href="{{ route('pacientes.index') }}">REGRESAR A  PACIENTES</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
--- resources/views/pacientes/index.blade.php
+++ resources/views/pacientes/index.blade.php
@@ -5,13 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+   <!-- <script src="https://cdn.tailwindcss.com"></script>  -->
+
+
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <script src="https://kit.fontawesome.com/424ce1386e.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Pagina principal de pacientes</h1>
+   
+    
+    <div class= "container p-8">
+        <div class= "row">
+            <div class= "col-md-8">
+                <div class= "card card-body">
+                    <h1>Pagina principal de pacientes</h1>
 
 
-    <table style="border: 1px solid black;">
+    <table class="table">
             <thead>
                 <tr>
                     <th style="border: 1px solid black;">id</th>
@@ -31,15 +42,15 @@
                     <td style="border: 1px solid black;">{{ $p->apellido }}</td>
                     <td style="border: 1px solid black;">{{ $p->celular }}</td>
                     <td  style="border: 1px solid black;">
-                                                             <a href="{{ route('pacientes.show',$p ->idPaciente) }}">VER</a>
-                                                             <a href="{{ route('pacientes.edit',$p ->idPaciente) }}">EDITAR</a>
-                                                            <form method= "POST" action="{{ route('pacientes.destroy',$p ->idPaciente) }}"
+                                                             <a href="{{ route('pacientes.show',$p ->idPaciente) }}"><i class="fa fa-eye"></i>VER</a>
+                                                             <a href="{{ route('pacientes.edit',$p ->idPaciente) }}" class="btn btn-secondary"><i class="fa fa-marker"></i>EDITAR</a>
+                                                            <form method= "POST" action="{{ route('pacientes.destroy',$p ->idPaciente) }}"class="btn btn-danger"
                                                                 onsubmit="return confirm('Esta seguro de borrar este Paciente?');"
                                                                 style ="display: inline-block">
                                                                 
                                                                 @csrf
                                                                 @method('Delete')
-                                                                <button type = "submit">BORRAR</button>
+                                                                <button type = "submit"><i class="fa fa-trash-alt"></i>BORRAR</button>
                                                             </form>
                                                             </td>
                 </tr>
@@ -50,10 +61,28 @@
 
     </table>
 
+    </div>
+        </div>
+            </div>
+
 
     {{$pacientes-> links()}}
 
     <br>
-    <a href="{{ route('pacientes.create') }}">CREAR UN  PACIENTES</a>
+  
+        
+    <div class= "container p-8">
+        <div class= "row">
+            <div class= "col-md-8">
+                <div class= "card card-body">
+                    <a href="{{ route('pacientes.create') }}">CREAR UN  PACIENTES</a>
+                </div>
+            </div>
+        </div>
+    
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>

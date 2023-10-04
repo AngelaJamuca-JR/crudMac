15ea9db89c27bfced5711176ccafe0151aa81b4b
antajara <tatisjamauk@gmail.com>
2023-10-04 15:39:43 -0500

    estilos

--- resources/views/pacientes/edit.blade.php
+++ resources/views/pacientes/edit.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
     <h1>EDITAR PACIENTE</h1>
@@ -14,7 +15,7 @@
         Nombre:  <input type="text" name="nombre" value={{ $paciente ->nombre }}><br>
         Apellido:<input type="text" name="apellido" value={{ $paciente ->apellido }}><br>
         Celular: <input type="text" name="celular" value={{ $paciente ->celular }}><br>
-        <input type="submit" value="Modificar Paciente">
+        <input type="submit" value="Modificar Paciente" >
     </form>
 
     <br>
